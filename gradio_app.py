aab88d6884c52440aab83046aafb0e0ac83ab534
yuanji-chucheng <yuanji-chucheng@users.noreply.github.com>
2026-09-12 23:29:30 +0800

    移除无用ultralytics源码文件夹，减少部署内存占用

--- gradio_app.py
+++ gradio_app.py
@@ -4,7 +4,7 @@ from rag_app.app import app
 if __name__ == "__main__":
     # 部署服务器关键参数：server_name="0.0.0.0"，监听所有网卡
     app.launch(
-        server_name="127.0.0.1",
+        server_name="0.0.0.0",
         server_port=7860,
         share=False
     )
